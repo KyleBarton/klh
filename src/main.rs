159c547b96f836f7243976ec87bfea955576a43c
Kyle Barton <kjbarton4@gmail.com>
2020-06-13 11:45:56 -0700

    significant refactoring. adding logging, accepting user input on a loop. Next need to add support for backspaces

--- src/main.rs
+++ src/main.rs
@@ -1,151 +1,112 @@
 extern crate termios;
 extern crate libc;
 
-use std::env;
-use std::fs::File;
 use std::io;
-use std::io::Error;
 use std::io::prelude::*;
-use std::path::Path;
 use termios::*;
+use std::fmt::Write;
+use std::io::Write as Otherwise;
+use std::fs::File;
 
+//backspace == ascii 127
 fn main() -> io::Result<()> {
     let std_fd = libc::STDIN_FILENO;
-    println!("Enter a single character: ");
-    let termios = Termios::from_fd(std_fd).unwrap();
-    let mut my_termios = termios.clone();
-    my_termios.c_lflag &= !(ICANON | ECHO);
-    tcsetattr(std_fd, TCSANOW, &mut my_termios).unwrap();
+    let termios = enable_canononical(std_fd);
+    println!("Begin typing! You're in klh");
 
-    let stdout = io::stdout();
-    let mut reader = io::stdin();
-    let mut buffer = [0;1];
-    println!("Hit a key!");
-    stdout.lock().flush().unwrap();
-    reader.read_exact(&mut buffer).unwrap();
-    println!("You have hit: {:?}", buffer[0] as char);
-    tcsetattr(std_fd, TCSANOW, & termios).unwrap();
+    let mut log = String::new();
+
+    //TODO string is not the struc we want to use here
+    let mut ed_buffer = String::new();
+
+    loop {
+        //TODO to be safe we should be injecting the fd here
+        let mut command_buffer = [0;1];
+        await_command(&mut command_buffer).unwrap();
+        if command_buffer[0] == 'Q' as u8 {
+            break;
+        }
+
+        process_command(&command_buffer, &mut ed_buffer).unwrap();
+
+        write!(
+            &mut log,
+            "Command entered was {:?} ===> {}\n",
+            command_buffer[0],
+            command_buffer[0] as char).unwrap();
+
+        display(&command_buffer);
+        //display_buffer(&ed_buffer);
+    }
+
+    let log_filename = "klh.log";
+    save_log(&log_filename, &log)?;
+
+    println!("\nexiting! Log of this session can be found in {}", log_filename);
+
+    disable_canonical(termios, std_fd);
+
     Ok(())
 }
-    // loop {
-    //     let mut buffer = [0;1];
-    //     let stdin = io::stdin();
-    //     let mut handle = stdin.lock();
-    //     handle.read_exact(&mut buffer);
-    //     //io::stdin().read_exact(&mut buffer)?;
-    //     print!("{:?}", buffer[0]);
-    //     io::stdout().flush();
-    //     if buffer[0] == 113 {
-    //         break;
-    //     }
-    //     text.push(buffer[0] as char);
-    // }
-    //     let input: i32 = std::io::stdin()
-    //     .bytes()
-    //     .next()
-    //     .and_then(|result| { result.ok() })
-    //     .map(|byte| byte as i32)
-    //     .unwrap();
-    // println!("Char entered is {:?}", input);
 
-    // print!("String entered is {} \n", text);
+/*So this has the obvious problem of coupling to command interpretation
+That is, display() implicitly has the "echo" action on all commands. What's nice here is we aren't redrawing
+every time.
+*/
+fn display(buffer: &[u8]) {
+    if buffer.len() != 1 {
+       panic!("Only accepting single-byte buffers at this time");
+    }
+    print!("{}", buffer[0] as char);
+    let stdout = io::stdout();
+    stdout.lock().flush().unwrap();
+}
 
-    // let mut buffer = [0;1];
-    // //let mut handle = stdin.lock();
-    // //handle.read_to_string(&mut buffer)?;
-    // io::stdout().flush();
+fn display_buffer(buffer: &String) {
+}
 
-    // // io::stdin()
-    // //     .read_line(&mut buffer)
-    // //     .expect("Failure to read line");
-    // // print!("$> {}", &buffer);
-    // print!("Text entered is {:?} \n", buffer);
-// use std::io::Read;
+fn enable_canononical(fd: i32) -> termios::Termios {
+    let termios = Termios::from_fd(fd).unwrap();
+    let mut my_termios = termios.clone();
+    my_termios.c_lflag &= !(ICANON | ECHO);
+    tcsetattr(fd, TCSANOW, &mut my_termios).unwrap();
+    return termios;
+}
 
-// let input: Option<i32> = std::io::stdin()
-//     .bytes() 
-//     .next()
-//     .and_then(|result| result.ok())
-//     .map(|byte| byte as i32);
+fn disable_canonical(term: Termios, fd: i32) {
+    tcsetattr(fd, TCSANOW, & term).unwrap();
+}
 
-// println!("{:?}", input);
-// fn main() {
-//     let stdin = 0; // couldn't get std::os::unix::io::FromRawFd to work 
-//     // on /dev/stdin or /dev/tty
-//     let termios = Termios::from_fd(stdin).unwrap();
-//     let mut new_termios = termios.clone();  // make a mutable copy of termios 
-//     // that we will modify
-//     new_termios.c_lflag &= !(ICANON | ECHO); // no echo and canonical mode
-//     tcsetattr(stdin, TCSANOW, &mut new_termios).unwrap();
-//     let stdout = io::stdout();
-//     let mut reader = io::stdin();
-//     let mut buffer = [0;1];  // read exactly one byte
-//     print!("Hit a key! ");
-//     stdout.lock().flush().unwrap();
-//     reader.read_exact(&mut buffer).unwrap();
-//     println!("You have hit: {:?}", buffer);
-//     tcsetattr(stdin, TCSANOW, & termios).unwrap();  // reset the stdin to 
-//     // original termios data
-// }
+//TODO we need to wrap std::io entirely here
+fn await_command(buffer: &mut[u8]) -> Result<(), std::io::Error> {
+    let mut reader = io::stdin();
+    match reader.read_exact(buffer) {
+        Err(e) => Err(e),
+        _ => Ok(())
+    }
+}
 
+fn save_log(filename: &str, log: &String) -> io::Result<()> {
+    let mut file = File::create(filename)?;
+    write!(&mut file, "{}", &log).unwrap();
+    Ok(())
+}
 
-// use std::io::{self, Read};
+fn process_command(command_buf: &[u8], ed_buf: &mut String) -> Result<(), std::io::Error> {
+    if command_buf.len() != 1 {
+        panic!("Command buf should only every be len 1");
+    }
+    let cmd = command_buf[0];
 
-// fn main() -> io::Result<()> {
-//     let mut buffer = String::new();
-//     let stdin = io::stdin();
-//     let mut handle = stdin.lock();
+    match cmd {
+        //lowercase
+        97..=122 => write!(ed_buf, "{}", cmd as char).unwrap(),
+        //uppercase
+        65..=90 => write!(ed_buf, "{}", cmd as char).unwrap(),
+        //ok turns out we want to do this by default right now
+        _ => write!(ed_buf, "{}", cmd as char).unwrap(),
+    };
 
-//     handle.read_to_string(&mut buffer)?;
-//     Ok(())
-// }
-// fn main_old() -> Result<(), io::Error> {
-//     let args: Vec<String> = env::args().collect();
-
-//     if args.len() != 2 {
-//         panic!("Can only take one input file as a parameter!");
-//     }
-//     // Ok this is fine, now let's actually open into a buffer
-//     let file_name = &args[1];
-//     let path = Path::new(&file_name);
-//     let display = path.display();
-//     let mut file = match File::open(&path) {
-//         Err(why) => panic!("Couldn't open file because {}!", why.to_string()),
-//         Ok(file) => file
-//     };
-//     let mut contents = String::new();
-// 	// Welp, anything past this is gonna need a view
-//     match file.read_to_string(&mut contents) {
-//         Err(why) => panic!("couldn't read {}: {}", display,
-//                                                    why.to_string()),
-//         Ok(_) => print!("Nice, sending the contents of {} to the view", display),
-//     };
-//     //view work
-//     let stdout = io::stdout().into_raw_mode()?;
-//     let backend = TermionBackend::new(stdout);
-//     let mut terminal = Terminal::new(backend)?;
-
-//     terminal.draw(|mut f| {
-//          let chunks = Layout::default()
-//             .direction(Direction::Vertical)
-//             .margin(1)
-//             .constraints(
-//                 [
-//                     Constraint::Percentage(10),
-//                     Constraint::Percentage(80),
-//                     Constraint::Percentage(10)
-//                 ].as_ref()
-//             )
-//             .split(f.size());
-//         let block = Block::default()
-//              .title("Block")
-//              .borders(Borders::ALL);
-//         f.render_widget(block, chunks[0]);
-//         let block = Block::default()
-//              .title("Block 2")
-//              .borders(Borders::ALL);
-//         f.render_widget(block, chunks[1]);
-//         print!("This is a bit of std output that is entered after rendering has occured lets see what happens");
-//     })
-// }
+    Ok(())
+}
 
