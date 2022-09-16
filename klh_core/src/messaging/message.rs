4470b404b4ea22fc20b348c1d19665f2f13d1c16
Kyle Barton <kjbarton4@gmail.com>
2022-09-15 20:55:50 -0700

    Document message. Make Message::new() crate private.
    Allow message to get_content multiple times.

--- klh_core/src/messaging/message.rs
+++ klh_core/src/messaging/message.rs
@@ -2,6 +2,12 @@ use core::fmt;
 
 use super::{MessageType, RequestResponder, MessageContent};
 
+/// The fundamental struct by which plugins accept data through the
+/// [Plugin::accept_message](crate::plugin::Plugin::accept_message)
+/// interface. The plugin contains the necessary information for a
+/// plugin to get the [MessageType] of the originating request, the
+/// [MessageContent] of the message, and a [RequestResponder] with
+/// which to send an asynchronous response.
 #[derive(Debug)]
 pub struct Message {
   message_type: MessageType,
@@ -11,7 +17,7 @@ pub struct Message {
 
 impl Message {
 
-  pub fn new(
+  pub(crate) fn new(
     message_type: MessageType,
     responder: Option<RequestResponder>,
     content: Option<MessageContent>,
@@ -23,16 +29,28 @@ impl Message {
     }
   }
 
+  /// Returns the [MessageType] associated with the Message.
   pub fn get_message_type(&self) -> MessageType {
     self.message_type
   }
 
+  /// Gets a one-time use responder with which to asynchronously
+  /// respond to the message. A `Message` instance returns [None] if
+  /// this function is called more than once.
   pub fn get_responder(&mut self) -> Option<RequestResponder> {
     self.responder.take()
   }
 
+  /// Gets the [MessageContent] of the message, if present. Otherwise,
+  /// returns [None].
   pub fn get_content(&mut self) -> Option<MessageContent> {
-    self.content.take()
+    match self.content.take() {
+      None => None,
+      Some(c) => {
+	self.content = Some(c.clone());
+	Some(c)
+      }
+    }
   }
 }
 
