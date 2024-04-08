16554ec27813e2a09ffa2d4f9484d497d9192fae
KyleBarton <kjbarton4@gmail.com>
2024-04-07 20:37:08 -0700

    Add a utility function to assert equality on message type id.

--- klh_core/src/plugins/diagnostics/mod.rs
+++ klh_core/src/plugins/diagnostics/mod.rs
@@ -41,29 +41,29 @@ impl Plugin for Diagnostics {
 
   fn accept_message(&mut self, mut message: Message) -> Result<(), MessageError> {
     debug!("[DIAGNOSTICS] Diagnostics received message {}", message);
-    match message.get_message_type() {
-      MessageType::Query(_) => {
-	warn!("[DIAGNOSTICS] message type not found: {}", &message.get_message_type());
-	Err(MessageError::PluginFailedToProcessMessage)
-      },
-      MessageType::Command(id) => {
-	if &id[0.."diagnostics::log_event".len()] == "diagnostics::log_event".as_bytes() {
-	  info!("[DIAGNOSTICS] Diagnostics plugin received a log event.");
-	}
-	if &id[0.."diagnostics::slow_bomb".len()] == "diagnostics::slow_bomb".as_bytes() {
-	  let content: models::SlowBombContent = message.get_content()
-	    .expect("Should have content")
-	    .deserialize()
-	    .expect("Should have slow bomb content");
-	  debug!("[DIAGNOSTICS] Diagnostics processing a slow bomb for {} seconds.", &content.interval_seconds);
-	  thread::sleep(time::Duration::from_secs(content.interval_seconds));
-	  let mut responder = message.get_responder()
-	    .expect("Should not have responded yet.");
-	  responder.respond(MessageContent::empty()).unwrap();
-	}
-	Ok(())
-      }
+    let message_type = message.get_message_type();
+
+    if message_type.id_equals_str("diagnostics::log_event") {
+      info!("[DIAGNOSTICS] Diagnostics plugin received a log event.");
     }
+
+    if message_type.id_equals_str("diagnostics::slow_bomb") {
+      let content: models::SlowBombContent = message.get_content()
+	.expect("Should have content")
+	.deserialize()
+	.expect("Should have slow bomb content");
+      debug!("[DIAGNOSTICS] Diagnostics processing a slow bomb for {} seconds.", &content.interval_seconds);
+      thread::sleep(time::Duration::from_secs(content.interval_seconds));
+      let mut responder = message.get_responder()
+	.expect("Should not have responded yet.");
+      responder.respond(MessageContent::empty()).unwrap();
+    }
+
+    else {
+      warn!("[DIAGNOSTICS] message type not found: {}", &message.get_message_type());
+    }
+    
+    Ok(())
   }
 
   fn list_message_types(&self) -> Vec<MessageType> {
