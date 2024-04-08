5a864f64f12f9192f1707582b9003a117b38fa35
KyleBarton <kjbarton4@gmail.com>
2024-04-07 21:57:09 -0700

    Build out the window object in display plugin

--- klh_core/src/plugins/display/models.rs
+++ klh_core/src/plugins/display/models.rs
@@ -3,10 +3,27 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct ListWindowsResponse {
-  pub list_as_string: String,
+  pub window_names: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct CreateWindowRequest {
   pub window_name: String,
+}
+
+
+pub struct Window {
+  pub name: String,
+  pub active_buffer_name: Option<String>,
+  pub associated_buffers_names: Vec<String>,
+}
+
+impl Window {
+  pub fn new(name: String) -> Self {
+    Self {
+      name,
+      active_buffer_name: None,
+      associated_buffers_names: Vec::new(),
+    }
+  }
 }
