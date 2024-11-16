98938e654e687a74521595bce562a73bd8b7f796
KyleBarton <kjbarton4@gmail.com>
2024-11-15 21:54:03 -0800

    Add buffer commands to add content.

--- klh_core/src/plugins/buffers/models.rs
+++ klh_core/src/plugins/buffers/models.rs
@@ -1,4 +1,3 @@
-
 use serde::{Serialize, Deserialize};
 
 // TODO separate this into models.rs, requests.rs, and responses.rs
@@ -8,10 +7,26 @@ use serde::{Serialize, Deserialize};
 // Eh deal with it when you have to. For now you might be able to avoid it.
 
 #[derive(Serialize, Deserialize)]
-pub struct CreateBufferContent {
+pub struct CreateBufferRequest {
   pub name: String,
 }
 
+/// A simple way to append a string of characters to the end of the buffer. No cursor/point considered here.
+#[derive(Serialize, Deserialize)]
+pub struct AppendStringToBufferRequest {
+  pub buffer_name: String,
+  pub content: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GetBufferRequest {
+  pub buffer_name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GetBufferResponse {
+  pub buffer: Option<Buffer>,
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct ListBuffersResponse {
@@ -19,14 +34,35 @@ pub struct ListBuffersResponse {
 }
 
 
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Buffer {
-  pub name: String
+  pub name: String,
+  pub content: BufferContent,
 }
 
 impl Buffer {
   pub fn new(name: String) -> Self {
     Self {
       name,
+      content: BufferContent::new(),
     }
+  }
+}
+
+// Placeholder for a way to store the content. For now basically wraps a string
+#[derive(Serialize, Deserialize, Clone)]
+pub struct BufferContent {
+  content: String,
+}
+
+impl BufferContent {
+  pub fn new() -> Self {
+    Self {
+      content: "".to_string(),
+    }
+  }
+
+  pub fn append(&mut self, content: String) {
+    self.content.push_str(&content);
   }
 }
